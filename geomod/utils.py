9f38250516317465224e12928c907be17f48a656
Denys Godwin <dgodwin@clarku.edu>
2023-12-04 05:55:57 +0000

    preliminary prediction complete in predict-example

--- geomod/utils.py
+++ geomod/utils.py
@@ -3,33 +3,31 @@
 import xarray as xr
 import numpy as np
 
-def driver_map_to_suitability(categorized_driver_maps, land_cover_map):
+def create_suitability_map_xarray(driver_maps, land_cover_map, weights=None):
     '''
-    reclassifies each driver map such that the grid cells of each
-    category of the driver map are assigned a percent-developed real number, obtained by comparing the driver map to the
-    beginning time land-cover map.
+    reclassifies continuous driver maps to categorical, leaves cateogorical maps as is
+    NOTE: Not needed for main predictor, this will be part of the pre-predictor data preparation pipeline 
+    Returns:
+        categorized_driver_maps
     '''
-
-def create_suitability_map(list_of_categorized_driver_maps, driver_map_weights, land_cover_map, strata_map):
-    '''
-    GEOMOD's suitability map is created in two steps. 
-    First, GEOMOD reclassifies each driver map such that the grid cells of each
-    category of the driver map are assigned a percent-developed real number, obtained by comparing the driver map to the
-    beginning time land-cover map. The percent-developed real number of each category in the driver map is computed as the ratio
-    of the quantity of developed grid cells of that category to the quantity of all grid cells of that category.
-    '''
-    suitability_maps = []
-    for driver_map in list_of_driver_maps:
-        suitability_maps.append(driver_map_to_suitability(driver_map, land_cover_maps)) # PLEASE note: it may be better to stack them all in xarray or something
-    
-
-### Alternatively, using something like xarray, where the array is something like [map_name, height, width] with each cell containing the value for that map name at that location:
-
-def create_suitability_map_xarray(xarray_of__categorized_driver_and_landcover_maps):
-    xarray_of_suitability_maps = None # do the math by relating dimensions of the xarray to each other
-
-
-
+    reclass_maps = {}
+    num_bands = driver_maps.shape[0]
+    for band in driver_maps['band']:
+        percent_developed = {}
+        bandarray = driver_maps.sel(band=band)
+        band_categories = np.unique(bandarray).tolist()
+        for category in band_categories:
+            category_cells = bandarray == category
+            developed_cells = (category_cells & (land_cover_map == 1)).sum().item()
+            total_cells = category_cells.sum().item()
+            percent_developed[category] = developed_cells / total_cells *100 if total_cells > 0 else 0
+        reclass_maps[str(band.values)] = percent_developed
+    driver_maps_as_suitability = driver_map_classification(driver_maps, reclass_maps)
+    if weights:
+        suitability_map = sum(driver_maps_as_suitability.sel(band=band) * weight for band, weight in weights.items())
+    else:
+        suitability_map = driver_maps_as_suitability.mean(dim='band')
+    return suitability_map
 
 def driver_map_classification(driver_maps, reclass_maps):
     '''
@@ -73,26 +71,26 @@ def reclassify_landcover_map(land_cover_map, class_of_interest):
 
 def get_edges(binary_xarray, constrain_to_nieghborhood):
 
-    # m
-    
-    borders = xr.zeros_like(class_array, dtype=int)
+  
+    x_size, y_size = binary_xarray.sizes['x'], binary_xarray.sizes['y']
+    borders = xr.zeros_like(binary_xarray, dtype=int)
 
-    # Get the size of the input array in both dimensions
-    x_size, y_size = class_array.sizes['x'], class_array.sizes['y']
+    binary_np = binary_xarray.values
+    borders_np = borders.values
 
-    # Iterate through each value
+    # Iterate and update using numpy arrays
     for i in range(x_size):
         for j in range(y_size):
-            # Select those which are in class 1
-            if class_array.loc[i, j] == 1:
-                # Define the range for slicing, handling edge cases
-                i_min, i_max = max(i-1, 0), min(i+2, x_size)
-                j_min, j_max = max(j-1, 0), min(j+2, y_size)
+            if binary_np[:, i, j] == 0:
+                i_min, i_max = max(i - 1, 0), min(i + 2, x_size)
+                j_min, j_max = max(j - 1, 0), min(j + 2, y_size)
 
-                # Get slice of all adjacent pixels
-                slice_adj = class_array.loc[i_min:i_max, j_min:j_max]
+                slice_adj = binary_np[:, i_min:i_max, j_min:j_max]
 
-                # Check if any belong to class 2
-                if np.any(slice_adj == 2):
-                    # Update borders array
-                    borders.loc[i, j] = 1
+                if np.any(slice_adj == 1):
+                    borders_np[:, i, j] = 1
+
+    # Convert the numpy array back to xarray DataArray
+    borders = xr.DataArray(borders_np, coords=binary_xarray.coords, dims=binary_xarray.dims)
+
+    return borders
